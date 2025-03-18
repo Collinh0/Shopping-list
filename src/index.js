b9b8c4b5ea127a3c1c37cf6f6a4505f89b0318f5
Collins <collowmartial@gmail.com>
2025-03-18 17:00:14 +0300

    original text files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("text")
+
+const add = document.getElementById("add")
+const mark = document.getElementById("mark")
+const clear = document.getElementById("clear")
+
+add.addEventListener("click", (event) => {
+    console.log("Clicked")
+});
+mark.addEventListener("click", (event) => {
+    console.log("Marked")
+});
+clear.addEventListener("click", (event) => {
+    console.log("Cleared")});
